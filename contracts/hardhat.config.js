5a07700c0699d4ec00d4b3c34fcb098393615df1
Master DAO <xmrtsolutions@gmail.com>
2026-05-07 20:57:30 -0600

    feat(deploy): add Sepolia + Etherscan verification to Hardhat config

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,46 +1,29 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+// Load env vars if .env exists
+try {
+  require("dotenv").config();
+} catch(e) {}
+
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
-
   for (const account of accounts) {
     console.log(account.address);
   }
 });
 
-task("balances", "Prints the list of AVAX account balances", async () => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    const balance = await ethers.provider.getBalance(account.address);
-    console.log(account.address, "has balance", balance.toString());
-  }
-});
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: {
     compilers: [
-      {
-        version: "0.5.16"
-      },
-      {
-        version: "0.6.2"
-      },
-      {
-        version: "0.6.4"
-      },
-      {
-        version: "0.7.0"
-      },
-      {
-        version: "0.8.0"
-      }
+      { version: "0.5.16" },
+      { version: "0.6.2" },
+      { version: "0.6.4" },
+      { version: "0.7.0" },
+      { version: "0.8.0" },
+      { version: "0.8.20" },
+      { version: "0.8.24" }
     ]
   },
   networks: {
@@ -52,22 +35,29 @@ module.exports = {
       url: 'http://localhost:9650/ext/bc/C/rpc',
       gasPrice: 470000000000,
       chainId: 43112,
-      accounts: ["0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027", 
-    ]
+      accounts: [process.env.PRIVATE_KEY || "0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027"]
     },
     fuji: {
       url: 'https://api.avax-test.network/ext/bc/C/rpc',
       gasPrice: 470000000000,
       chainId: 43113,
-      accounts: [
-    ]
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
     },
     mainnet: {
       url: 'https://api.avax.network/ext/bc/C/rpc',
       gasPrice: 470000000000,
       chainId: 43114,
-      accounts: [
-    ]
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC || 'https://rpc.sepolia.org',
+      chainId: 11155111,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY || ""
     }
   }
 };
